9f9984e3814f6dd95a81568e486be249cb635672
Gonza <gonzaemma1992@gmail.com>
2023-06-28 22:43:22 -0300

    Vite project starts

---
+++ Partido-politico/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <NavBar/>
+        <h1>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Partido-politico/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+function NavBar(){
+    return <div>
+        Soy una navbar
+    </div>
+}
+
+export default NavBar;
